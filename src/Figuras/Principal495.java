b9185af6d1dff179d88c1a86201bb12da6ff3c84
alcchan <motalvaro@unal.edu.co>
2025-02-12 00:00:45 -0500

    Ejercicio 8.3 pagina 495

---
+++ src/Figuras/Principal495.java
@@ -0,0 +1,16 @@
+package Figuras;
+
+
+public class Principal495 {
+
+    
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+        miVentanaPrincipal.setLocationRelativeTo(null);
+        
+        }
+
+}
+
